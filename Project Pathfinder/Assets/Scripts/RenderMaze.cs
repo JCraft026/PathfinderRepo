30447e185fe517ada766c1553e35e7e38e06d2c5
Kirkorus <kirklandt01@gmail.com>
2022-10-16 17:20:46 -0500

    Update RenderMaze.cs
    Made a slider bar for the density of the maze generation. My recommendation is to keep it between 20-30?

--- Project Pathfinder/Assets/Scripts/RenderMaze.cs
+++ Project Pathfinder/Assets/Scripts/RenderMaze.cs
@@ -14,6 +14,10 @@ public class RenderMaze : MonoBehaviour
     private int mazeHeight = 10;
 
     [SerializeField]
+    [Range(1, 100)]
+    private int mazeDensity = 26;
+
+    [SerializeField]
     private float cellSize = 1f;
 
     [SerializeField]
@@ -21,6 +25,8 @@ public class RenderMaze : MonoBehaviour
 
     [SerializeField]
     private Transform floorPrefab = null;
+
+    int randomNumber = 0; // Random number to determine if a wall generates or not
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +39,8 @@ public class RenderMaze : MonoBehaviour
     }
 
     // Render the complete maze within the scene
-    private void Render(WallStatus[,] mazeData){
+    private void Render(WallStatus[,] mazeData)
+    {
         WallStatus currentCell = new WallStatus(); // Current maze cell being rendered
         Vector2 scenePosition  = new Vector2();    // x,y position in the scene
         var mazeFloor          = Instantiate(floorPrefab, transform);
@@ -43,12 +50,10 @@ public class RenderMaze : MonoBehaviour
         mazeFloor.localScale = new Vector2(cellSize * (mazeWidth), cellSize * (mazeHeight));
 
         // Adujust the maze floor in relation to the maze cells
-        if(mazeHeight % 2 == 0)
-        {
+        if(mazeHeight % 2 == 0){
             mazeFloor.position += new Vector3(0, -cellSize / 2, 0);
         }
-        if(mazeWidth % 2 == 0)
-        {
+        if(mazeWidth % 2 == 0){
             mazeFloor.position += new Vector3(-cellSize / 2, 0, 0);
         }
 
@@ -57,16 +62,37 @@ public class RenderMaze : MonoBehaviour
             for (int i = 0; i < mazeWidth; i++){
                 currentCell = mazeData[i,j];
                 scenePosition = new Vector2(cellSize * (-mazeWidth / 2 + i), cellSize * (-mazeHeight / 2 + j));
+                randomNumber  = Random.Range(1, mazeDensity + 1); // Provides RNG for maze generation
 
                 // Render the top wall of a maze cell
-                if(currentCell.HasFlag(WallStatus.TOP)){
-                    var topWall        = Instantiate(wallPrefab, transform) as Transform;
-                    topWall.position   = scenePosition + new Vector2(0, cellSize / 2);
-                    topWall.localScale = new Vector2(cellSize, topWall.localScale.y);
+                if(j != mazeHeight - 1){
+                    // Potentially render the top walls in between the top and bottom of the maze
+                    if(currentCell.HasFlag(WallStatus.TOP) && randomNumber != 1){
+                        var topWall        = Instantiate(wallPrefab, transform) as Transform;
+                        topWall.position   = scenePosition + new Vector2(0, cellSize / 2);
+                        topWall.localScale = new Vector2(cellSize, topWall.localScale.y);
+                    }
+                }
+                else{
+                    // Render the top walls at the top of the maze
+                    if(currentCell.HasFlag(WallStatus.TOP)){
+                        var topWall        = Instantiate(wallPrefab, transform) as Transform;
+                        topWall.position   = scenePosition + new Vector2(0, cellSize / 2);
+                        topWall.localScale = new Vector2(cellSize, topWall.localScale.y);
+                    }
                 }
 
                 // Render the left wall of a maze cell
-                if(currentCell.HasFlag(WallStatus.LEFT)){
+                if(i != 0){
+                    // Potentially render left walls inbetween the left and right of maze
+                    if(currentCell.HasFlag(WallStatus.LEFT) && randomNumber != 1){
+                        var leftWall         = Instantiate(wallPrefab, transform) as Transform;
+                        leftWall.position    = scenePosition + new Vector2(-cellSize / 2, 0);
+                        leftWall.localScale  = new Vector2(cellSize, leftWall.localScale.y);
+                        leftWall.eulerAngles = new Vector3(0, 180, 90);
+                    }
+                }
+                else{
                     var leftWall         = Instantiate(wallPrefab, transform) as Transform;
                     leftWall.position    = scenePosition + new Vector2(-cellSize / 2, 0);
                     leftWall.localScale  = new Vector2(cellSize, leftWall.localScale.y);
@@ -75,8 +101,8 @@ public class RenderMaze : MonoBehaviour
 
                 // Render the bottom wall of a maze cell if the current cell is in the bottom row
                 if(j == 0){
-                    if (currentCell.HasFlag(WallStatus.BOTTOM))
-                    {
+                    // Render the bottom wall of the maze
+                    if(currentCell.HasFlag(WallStatus.BOTTOM)){
                         var bottomWall        = Instantiate(wallPrefab, transform) as Transform;
                         bottomWall.position   = scenePosition + new Vector2(0, -cellSize / 2);
                         bottomWall.localScale = new Vector2(cellSize, bottomWall.localScale.y);
@@ -85,8 +111,7 @@ public class RenderMaze : MonoBehaviour
 
                 // Render the right wall of a maze cell if the current cell is in the right most column
                 if(i == mazeWidth - 1){
-                    if (currentCell.HasFlag(WallStatus.RIGHT))
-                    {
+                    if (currentCell.HasFlag(WallStatus.RIGHT)){
                         var rightWall         = Instantiate(wallPrefab, transform) as Transform;
                         rightWall.position    = scenePosition + new Vector2(+cellSize / 2, 0);
                         rightWall.localScale  = new Vector2(cellSize, rightWall.localScale.y);
