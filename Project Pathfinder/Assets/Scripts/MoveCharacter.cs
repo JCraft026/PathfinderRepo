dfe7ee50daebdffe91ab8e78ad8f07b37f8268f4
JCraft026 <jcraft111@yahoo.com>
2022-10-16 17:48:05 -0500

    Created Runner Animations

---
+++ Project Pathfinder/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float moveSpeed = 5f;  // Speed at which the character is moving
+    public Rigidbody2D rigidBody; // Character's RigidBody
+    Vector2 movementInput;        // Character's current input direction
+
+    // Update is called once per frame
+    void Update(){
+        // Get current input data
+        movementInput.x = Input.GetAxisRaw("Horizontal"); // Returns 0 if idle, 1 if right, -1 if left
+        movementInput.y = Input.GetAxisRaw("Vertical");   // Returns 0 if idle, 1 if up, -1 if down
+        
+    }
+
+    // FixedUpdate calling frequency is based on a set timer
+    void FixedUpdate(){
+        // Move the character based on the current character position, the input data, the move speed, and the elapesed time since the last function call
+        rigidBody.MovePosition(rigidBody.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
