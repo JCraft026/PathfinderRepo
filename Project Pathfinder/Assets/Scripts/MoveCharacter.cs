45eacf362045257ca03a456e55cdd234534075d5
JCraft026 <jcraft111@yahoo.com>
2022-10-19 15:50:51 -0500

    Finished implementing basic character movement

--- Project Pathfinder/Assets/Scripts/MoveCharacter.cs
+++ Project Pathfinder/Assets/Scripts/MoveCharacter.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+static class Constants{
+    public const float FORWARD  = 1; // Character facing forward
+    public const float LEFT     = 2; // Character facing left
+    public const float BACKWARD = 3; // Character facing backward
+    public const float RIGHT    = 4; // Character facing right
+}
+
 public class MoveCharacter : MonoBehaviour
 {
-    public float moveSpeed = 5f;   // Speed at which the character is moving
-    public Rigidbody2D rigidBody;  // Character's RigidBody
-    Vector2 movementInput;         // Character's current input direction
-    public Animator animator;      // Character's animator manager
+    public float moveSpeed = 5f,  // Speed at which the character needs to move
+                 facingDirection; // Direction the character should face after movement
+    public Rigidbody2D rigidBody; // Character's RigidBody
+    Vector2 movementInput;        // Character's current input direction
+    public Animator animator;     // Character's animator manager
 
     // Update is called once per frame
     void Update(){
@@ -15,14 +23,32 @@ public class MoveCharacter : MonoBehaviour
         movementInput.x = Input.GetAxisRaw("Horizontal"); // Returns 0 if idle, 1 if right, -1 if left
         movementInput.y = Input.GetAxisRaw("Vertical");   // Returns 0 if idle, 1 if up, -1 if down
 
+        // Set character facing direction
+        if(movementInput.x == 0 && movementInput.y == -1){
+            facingDirection = Constants.FORWARD;
+        }
+        else if(movementInput.x == -1 && movementInput.y == 0){
+            facingDirection = Constants.LEFT;
+        }
+        else if(movementInput.x == 0 && movementInput.y == 1){
+            facingDirection = Constants.BACKWARD;
+        }
+        else if(movementInput.x == 1 && movementInput.y == 0){
+            facingDirection = Constants.RIGHT;
+        }
+
         // Communicate movement values with the animator controller
         animator.SetFloat("Horizontal Movement", movementInput.x);
         animator.SetFloat("Vertical Movement", movementInput.y);
         animator.SetFloat("Movement Speed", movementInput.sqrMagnitude); // Set the speed to the squared length of the movementInput vector
+        animator.SetFloat("Facing Direction", facingDirection);
     }
 
     // FixedUpdate calling frequency is based on a set timer
     void FixedUpdate(){
+        // Make movement speed equal in all directions
+        movementInput.Normalize();
+
         // Move the character based on the current character position, the input data, the move speed, and the elapesed time since the last function call
         rigidBody.MovePosition(rigidBody.position + movementInput * moveSpeed * Time.fixedDeltaTime);
     }
