c5f99892abcf9ab9039411f16b0754568f69886c
JCraft026 <jcraft111@yahoo.com>
2022-10-19 13:40:52 -0500

    Implemented basic animations

--- Project Pathfinder/Assets/Scripts/MoveCharacter.cs
+++ Project Pathfinder/Assets/Scripts/MoveCharacter.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour
 {
-    public float moveSpeed = 5f;  // Speed at which the character is moving
-    public Rigidbody2D rigidBody; // Character's RigidBody
-    Vector2 movementInput;        // Character's current input direction
+    public float moveSpeed = 5f;   // Speed at which the character is moving
+    public Rigidbody2D rigidBody;  // Character's RigidBody
+    Vector2 movementInput;         // Character's current input direction
+    public Animator animator;      // Character's animator manager
 
     // Update is called once per frame
     void Update(){
         // Get current input data
         movementInput.x = Input.GetAxisRaw("Horizontal"); // Returns 0 if idle, 1 if right, -1 if left
         movementInput.y = Input.GetAxisRaw("Vertical");   // Returns 0 if idle, 1 if up, -1 if down
-        
+
+        // Communicate movement values with the animator controller
+        animator.SetFloat("Horizontal Movement", movementInput.x);
+        animator.SetFloat("Vertical Movement", movementInput.y);
+        animator.SetFloat("Movement Speed", movementInput.sqrMagnitude); // Set the speed to the squared length of the movementInput vector
     }
 
     // FixedUpdate calling frequency is based on a set timer
