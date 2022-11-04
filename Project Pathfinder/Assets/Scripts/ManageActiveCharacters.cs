c579781204ee1039cb4091756a6a18ca1128bba9
JCraft026 <jcraft111@yahoo.com>
2022-11-04 15:51:03 -0500

    Added some psuedocode for guard switching

--- Project Pathfinder/Assets/Scripts/ManageActiveCharacters.cs
+++ Project Pathfinder/Assets/Scripts/ManageActiveCharacters.cs
@@ -12,11 +12,42 @@ public class ManageActiveCharacters : NetworkBehaviour
     public override void OnStartAuthority()
     {
         base.OnStartAuthority();
+        /*
+        if(isRunner){
+            cameraHolder.SetActive(true);
+        }
+        else{
+            InitializeGuardIdentification();
+            activeGuardId = CustomNetwoorkManager.initalActiveGuardId
+            if(guardId == activeGuardId){
+                cameraHolder.SetActive(true);
+            }
+        }
+        */
         cameraHolder.SetActive(true);
     }
 
     public void Update()
     {
+        /*
+        if(Input.GetKeyDown("space")){
+            if(activeGuardId == 3){
+                nextActiveGuardId = 1;
+            }
+            else{
+                nextActiveGuardId++;
+            }
+            if(guardId == activeGuardId){
+                cameraHolder.SetActive(false);
+            }
+            else if(guardId == nextActiveGuardId){
+                CustomNetorkManager.ChangeActiveGuard(activeGuardId, nextActiveGuardId);
+                cameraHolder.SetActive(true);
+            }
+            activeGuardId = nextActiveGuardId;
+        }
+        cameraHolder.transform.position = transform.position + offset;
+        */
         cameraHolder.transform.position = transform.position + offset;
     }
 }
