a816204f513ed6e7552b6fed162420231d405e70
CalebFrancis <caleb.francis@students.pcci.edu>
2023-02-02 20:18:49 -0600

    commit

--- Project Pathfinder/Assets/Scripts/Scene Scripts/PauseGame.cs
+++ Project Pathfinder/Assets/Scripts/Scene Scripts/PauseGame.cs
@@ -1,34 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Mirror;
 using UnityEngine.SceneManagement;
 
-public class PauseGame : MonoBehaviour
+public class PauseGame : NetworkBehaviour
 {
     public GameObject PauseCanvas;
 
     public void Awake()
     {
-        PauseCanvas = GameObject.Find("PauseCanvas");
+        //PauseCanvas = GameObject.Find("PauseCanvas");
+        PauseCanvas = FindPauseCanvas();
         PauseCanvas.SetActive(false);
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+
+        if (Input.GetKeyDown(KeyCode.Escape) && isLocalPlayer)
         {
-            Application.Quit();
-            PauseCanvas.gameObject.SetActive(!PauseCanvas.gameObject.activeSelf);
+            Debug.Log(this.gameObject.name + " reacted to escape key");
+            //PauseCanvas.gameObject.SetActive(!PauseCanvas.gameObject.activeSelf);
+            if(PauseCanvas.gameObject.activeSelf == false)
+            {
+                OpenPauseCanvas();
+            }
+            else
+            {
+                ClosePauseCanvas();
+            }
+
         }
+    }
+
+    public GameObject FindPauseCanvas()
+    {
+       PauseCanvas = SceneManager.GetActiveScene()
+                                .GetRootGameObjects()
+                                .FirstOrDefault<GameObject>(x => x.name == "PauseCanvas");
+        if(PauseCanvas == null)
+        {
+            throw(new Exception("Could locate exit game menu"));
+        }
+        else
+            return PauseCanvas;
     }
 
     public void OpenPauseCanvas()
     {
+        Debug.Log("Open Exit Menu");
         PauseCanvas.SetActive(true);
     }
 
     public void ClosePauseCanvas()
     {
+        Debug.Log("Close Exit Menu");
         PauseCanvas.SetActive(false);
     }
 
@@ -39,6 +68,8 @@ public class PauseGame : MonoBehaviour
 
     public void ExitGame(int index)
     {
+        NetworkClient.Shutdown();
+        NetworkServer.Shutdown();
         Application.Quit();
         SceneManager.LoadScene(index);
     }
