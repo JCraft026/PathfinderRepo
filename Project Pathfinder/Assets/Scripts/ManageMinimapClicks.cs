081d45e448e216706611d8fef01dbbe0772c4b4c
JCraft026 <jcraft111@yahoo.com>
2023-03-04 19:35:44 -0600

    update

--- Project Pathfinder/Assets/Scripts/ManageMinimapClicks.cs
+++ Project Pathfinder/Assets/Scripts/ManageMinimapClicks.cs
@@ -20,34 +20,37 @@ public class ManageMinimapClicks : MonoBehaviour
     // Initiate guard fast travel if the minimap floor parent object is clicked
     void OnMouseDown()
     {
-        int activeGuardId = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Chaser")).GetComponent<ManageActiveCharacters>().activeGuardId;
-                                  // Guard ID of the current active guard
-        GameObject guard  = null; // Active guard object
+        if(!CustomNetworkManager.isRunner){
+            int activeGuardId = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Chaser")).GetComponent<ManageActiveCharacters>().activeGuardId;
+                                    // Guard ID of the current active guard
+            GameObject guard  = null; // Active guard object
 
-        if(activeGuardId == ManageActiveCharactersConstants.CHASER){
-            guard = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Chaser"));
-        }
-        else if(activeGuardId == ManageActiveCharactersConstants.ENGINEER){
-            guard = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Engineer"));
-        }
-        else{
-            guard = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Trapper"));
+            if(activeGuardId == ManageActiveCharactersConstants.CHASER){
+                guard = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Chaser"));
+            }
+            else if(activeGuardId == ManageActiveCharactersConstants.ENGINEER){
+                guard = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Engineer"));
+            }
+            else{
+                guard = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Trapper"));
+            }
+
+            // Initiate guard fast travel if the current guard isnt already fast traveling
+            if(!guard.GetComponent<Animator>().GetBool("Fast Travel Started"))
+            {
+                var mazeCell = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("mcf" + gameObject.name.Substring(2)));
+                ManageFastTravel.FastTravel(activeGuardId, mazeCell.transform.position);
+            }
         }
 
-        // Initiate guard fast travel if the current guard isnt already fast traveling
-        if(!CustomNetworkManager.isRunner && !guard.GetComponent<Animator>().GetBool("Fast Travel Started"))
-        {
-            var mazeCell = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("mcf" + gameObject.name.Substring(2)));
-            ManageFastTravel.FastTravel(activeGuardId, mazeCell.transform.position);
-        }
     }
 
     void OnMouseOver(){
-        var activeGuardId = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Chaser")).GetComponent<ManageActiveCharacters>().activeGuardId;
-                // Guard ID of the current active guard
-
         // Change minimap cell color based on the current active guard
         if(!CustomNetworkManager.isRunner){
+            var activeGuardId = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(gObject => gObject.name.Contains("Chaser")).GetComponent<ManageActiveCharacters>().activeGuardId;
+                    // Guard ID of the current active guard
+
             if(activeGuardId == ManageActiveCharactersConstants.CHASER){
                 spriteRenderer.color = Color.green;
             }
