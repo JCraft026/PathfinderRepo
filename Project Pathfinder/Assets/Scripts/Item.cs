0db07a3f8efcf53bcabed58e6fd524edfd8aa7d9
CalebFrancis <caleb.francis@students.pcci.edu>
2023-03-11 15:34:09 -0600

    added key spawns

--- Project Pathfinder/Assets/Scripts/Item.cs
+++ Project Pathfinder/Assets/Scripts/Item.cs
@@ -112,12 +112,35 @@ public class Item
                 break;
 
         }
-        if(item.isStackable()){
+        item.amount = 1;
+        /*if(item.isStackable()){
             item.amount = UnityEngine.Random.Range(1,9);
         }
         else{
             item.amount = 1;
-        }
+        }*/
         return item;
+    }
+
+    public static Item GetKey(int keyNum)
+    {
+        Item key = new Item();
+        switch(keyNum)
+        {
+            case 0:
+                key.itemType = Item.ItemType.Keys_0;
+            break;
+            case 1:
+                key.itemType = Item.ItemType.Keys_1;
+            break;
+            case 2:
+                key.itemType = Item.ItemType.Keys_2;
+            break;
+            case 3:
+                key.itemType = Item.ItemType.Keys_3;
+            break;
+        }
+        key.amount = 1;
+        return key;
     }
 }
