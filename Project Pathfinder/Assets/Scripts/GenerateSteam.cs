28b748c0dc46b00cb3573ef7b8e5910e025b02ab
Kirkorus <kirklandt01@gmail.com>
2023-03-22 16:50:30 -0500

    Working Steam Generators (not fixable Yet)

--- Project Pathfinder/Assets/Scripts/GenerateSteam.cs
+++ Project Pathfinder/Assets/Scripts/GenerateSteam.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using TMPro;
 
 public class GenerateSteam : MonoBehaviour
 {
-    public static float steam = 0;   // Amount of steam available for use
+    public static float steam = 90;   // Amount of steam available for use
     public int displaySteam;         // Amount of steam displayed
     private float steamLimit = 101f; // Total steam limit
     private float steamMultiplier;   // Variable steam generation
     public int generatorCount = 3; // Inital number of generators
+    private int toSubtract = 0;
 
     // Update is called once per frame
     void Update()
@@ -18,7 +20,7 @@ public class GenerateSteam : MonoBehaviour
         if(steam < steamLimit){
             displaySteam = (int)steam;
             gameObject.GetComponent<TextMeshProUGUI>().text = displaySteam.ToString();
-            switch(generatorCount){
+            switch(generatorCount - subtractBrokenGenerators()){
                 case 0: 
                     steamMultiplier = 0f;
                     break;
@@ -37,5 +39,18 @@ public class GenerateSteam : MonoBehaviour
             }
             steam += Time.deltaTime * steamMultiplier;
         }
+    }
+
+    public int subtractBrokenGenerators(){
+        toSubtract = 0;
+        List<GameObject> allGenerators = Resources.FindObjectsOfTypeAll<GameObject>()
+                                            .Where<GameObject>(x => 
+                                                x.GetComponent<GeneratorController>() != null)
+                                            .ToList();
+        allGenerators.ForEach(x =>{ 
+            if(x.GetComponent<Animator>().GetBool("IsBusted") == true) 
+                toSubtract += 1;
+        });
+        return toSubtract;
     }
 }
