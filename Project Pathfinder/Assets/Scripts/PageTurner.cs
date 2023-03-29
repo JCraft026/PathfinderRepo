2c47e335bce7a963bf74e912bff405e0a898d8cc
AnneWithAnELee <anne.lee@students.pcci.edu>
2023-03-29 16:52:05 -0700

    Better

--- Project Pathfinder/Assets/Scripts/PageTurner.cs
+++ Project Pathfinder/Assets/Scripts/PageTurner.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class PageTurner : MonoBehaviour
 {
-    public void TurnRunnerPage()
+    public GameObject RunnerPage;
+    public GameObject EscapeProcessPage;
+    public GameObject ItemPage;
+    public GameObject GeneralGuardPage;
+
+    public void Awake()
     {
-        CanvasRenderer.FindObjectOfType<GameObject>(GameObject.Find("RunnerPage"));
+        RunnerPage             = GameObject.Find("RunnerPage");
+        EscapeProcessPage      = GameObject.Find("EscapeProcessPage");
+        ItemPage               = GameObject.Find("ItemPage");
+        GeneralGuardPage       = GameObject.Find("GeneralGuardPage");
     }
 
-    public void TurnEscapePage()
+    public void TurnRunnerPageForward()
     {
-
+        RunnerPage.SetActive(false);
     }
 
-    public void TurnItemPage()
+    public void TurnEscapePageBack()
     {
+        RunnerPage.SetActive(true);
+    }
 
+    public void TurnEscapePageForward()
+    {
+        EscapeProcessPage.SetActive(false);
+    }
+
+    public void TurnItemPageBack()
+    {
+        EscapeProcessPage.SetActive(true);
+    }
+    public void TurnItemPageForward()
+    {
+        ItemPage.SetActive(false);
     }
 }
