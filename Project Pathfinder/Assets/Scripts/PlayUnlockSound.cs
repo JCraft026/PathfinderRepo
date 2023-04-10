1686a833ed5e1ea84776ad53f208ebd224cc5466
CalebFrancis <caleb.francis@students.pcci.edu>
2023-04-10 16:00:45 -0500

    Added alarm sound when runner opens door

--- Project Pathfinder/Assets/Scripts/PlayUnlockSound.cs
+++ Project Pathfinder/Assets/Scripts/PlayUnlockSound.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayUnlockSound : MonoBehaviour
 {
     public AudioSource unlockSound;
 
-    // Plays the unlock sound when the KeyHole (Unlocked) gameobject is enabled
+    // Plays the unlock sound
     void Start()
     {
         unlockSound.Play();
+
+        StartCoroutine(coroutine_PlayGuardAlarm());
+    }
+
+    // Plays the Guardmaster alarm sound
+    IEnumerator coroutine_PlayGuardAlarm()
+    {
+        var alarm = GameObject.Find("GuardAlarmSound").GetComponent<AudioSource>();
+        if(CustomNetworkManager.isRunner == false && alarm.isPlaying == false)
+        {
+            alarm.Play();
+            Debug.Log("CommandManager: Alarm is playing");
+            yield return new WaitForSeconds(2);
+            Debug.Log("CommandManager: Alarm is no longer playing");
+            alarm.Stop();
+        }
     }
 }
